38dcc89747d7bb60b8b9ffa7d39fd5941b6e7654
omarhesham1997 <36241516+omarhesham1997@users.noreply.github.com>
2019-06-29 18:25:43 +0200

    Fixing degree cent bugs
    Co-Authored-By: moerfan97 <moerfan97@users.noreply.github.com>

--- Graph Algorithms/centrality.cpp
+++ Graph Algorithms/centrality.cpp
@@ -8,63 +8,26 @@
 
 using namespace std;
    
-// This class represents a directed graph using 
-// adjacency list representation 
-class Graph 
-{ 
-    int V;    // No. of vertices 
-	// unweighted gragh for centraility
-	list <int> *uadj;
-  
-public: 
-    Graph(int V);  // Constructor 
-  
-    // function to add an edge to graph 
-   
-	  void addEdge(int a, int b);
-  
-	  //degree of centality
-	   int degreeOfCentrality(int s);
-}; 
-  
-// Allocates memory for adjacency list 
-Graph::Graph(int V) 
-{ 
-    this->V = V;  
-	  uadj =new list<int> [V];
-} 
-  
-void Graph::addEdge(int a, int b)
+int main ()
 {
-	uadj[a].push_back(b); 
-	uadj[b].push_back(a); 
+int v,e;
+int a,b,c;
+cin>>v>>e;
+
+vector <vector<int>> m (v);
+
+
+
+for(int i=0;i<e;i++)
+{
+	cin>>a>>b>>c;
+	m[a].push_back(b);
+	m[b].push_back(a);
 }
 
-
-int Graph::degreeOfCentrality(int src)
+for(int i=0;i<v;i++)
 {
-	return uadj[src].size();
+	cout<<m[i].size()<<"\n";
 }
-
-void main ()
-{
-	int V ,m; 
-	int a,b;
-	cin>>V>>m;
-  Graph g(V);
-
-	//for unweighted graph
-	for (int i = 0; i < m; i++)
-	{
-		cin>>a>>b;
-		g.addEdge(a,b);
-	}
-	/// this section for degree of centrality
-
-	for (int i=0; i < V; i++)
-	{
-		a=g.degreeOfCentrality(i);
-		cout<<i<<"\t"<<a<<"\n";
-	}
-	
+	return 0;
 }
